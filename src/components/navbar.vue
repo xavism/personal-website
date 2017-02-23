b437d3985c1bf6e172ea11b8e5a10e328cef55ed
Xavi Sánchez Mir <xavier.sanchez@tokiota.com>
2017-02-23 09:57:23 +0100

    Binding Navbar component

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -4,17 +4,11 @@
       <router-link class="nav-item" to="/">
         <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma logo">
       </router-link>
-       <router-link class="nav-item is-tab" to="/dashboard">
+      <router-link v-for="tab in tabs" class="nav-item is-tab" v-bind:to="tab.path">
         <span class="icon">
-            <i class="fa fa-rebel"></i>
+            <i v-bind:class="tab.icon"></i>
         </span>
-        <span>Dasboard</span>
-      </router-link>
-      <router-link class="nav-item is-tab" to="/projects">
-        <span class="icon">
-            <i class="fa fa-list-alt"></i>
-        </span>
-        <span>Projects List</span>
+        <span> {{ tab.title }} </span>
       </router-link>
     </div>
 
@@ -29,14 +23,14 @@
     <!-- This "nav-menu" is hidden on mobile -->
     <!-- Add the modifier "is-active" to display it on mobile -->
     <div class="nav-right nav-menu">
-      <a class="nav-item">
+      <a class="nav-item" target="_blank" v-bind:href="github.protocol + '://' + github.domain + '/' + github.username + '/' + github.repository">
         <span class="icon">
           <i class="fa fa-github"></i>
         </span>
       </a>
 
       <span class="nav-item">
-        <a class="button is-primary">
+        <a class="button is-primary" v-bind:href="github.protocol + '://' + github.domain + '/' + github.username + '/' + github.repository + github.download">
           <span class="icon">
             <i class="fa fa-download"></i>
           </span>
@@ -53,9 +47,24 @@ export default {
   data () {
     return {
       github: {
-        link: 'https://github.com',
-        username: 'xavism'
-      }
+        protocol: 'https',
+        domain: 'github.com',
+        username: 'xavism',
+        repository: 'vuesetup',
+        download: '/archive/master.zip'
+      },
+      tabs: [
+        {
+          title: 'Dasboard',
+          path: '/dashboard',
+          icon: 'fa fa-rebel'
+        },
+        {
+          title: 'Projects List',
+          path: '/projects',
+          icon: 'fa fa-list-alt'
+        }
+      ]
     }
   }
 }
